a732379fb1e40246a121be03d0c81facd15c222e
NestorAlban <albquenes7294@gmail.com>
2022-11-01 00:44:53 -0500

    01_november_2022_00_44

--- main.js
+++ main.js
@@ -1,11 +1,107 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarroIcon = document.querySelector('.navbar-shopping-cart');
 const deskMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 
-menuEmail.addEventListener('click', toggleMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarroIcon.addEventListener('click', toggleCarroAside);
 
-function toggleMenu(){
-    console.log('Click');
+function toggleDesktopMenu(){
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose){
+        aside.classList.add('inactive');
+    }
+    
     deskMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(){
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose){
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarroAside(){
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+    const ismenuEmailClose = menuEmail.classList.contains('inactive');
+
+    if (!isMobileMenuClose){
+        mobileMenu.classList.add('inactive');
+    }
+    if (!ismenuEmailClose){
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Compu',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+
+function renderProductsList(prodList){
+    for (product of prodList){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        // product = {name, price, image}
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-card');
+        
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+
+}
+
+renderProductsList(productList);
